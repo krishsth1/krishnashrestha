3389d865f987f5b8eb32aa84fc1ec3bfa809ac40
Krishna Kumar Shrestha <45503377+krishsth1@users.noreply.github.com>
2020-08-24 22:27:07 +0545

    Add files via upload

--- Eco/R/p.R
+++ Eco/R/p.R
@@ -18,5 +18,8 @@ p<- function(df,col){
     geom_line(size = 0.64, colour =col) +
     labs(title = "Labour Unit VS Total Production",  caption = "Cobb-Douglas Production Function") +
     geom_point()+
-    scale_x_continuous(breaks = L) + xlab("Labour Unit")+ylab("Total Production")
+    scale_x_continuous(breaks = L) + 
+    xlab("Labour Unit")+
+    ylab("Total Production")
+  return(plot1)
 }
