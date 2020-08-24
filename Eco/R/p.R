1729e68e9c284e957dd45c8e5ce6494ecf31ae2e
Krishna Kumar Shrestha <45503377+krishsth1@users.noreply.github.com>
2020-08-24 22:24:13 +0545

    chage

---
+++ Eco/R/p.R
@@ -0,0 +1,22 @@
+#' it apply
+#' df data frame
+#' col is colour to line plot 
+
+#' @export
+
+
+
+
+
+p<- function(df,col){
+  
+  
+  data=df
+  library(ggplot2)
+  plot1<-ggplot(data) +
+    aes(x = L, y = TP) +
+    geom_line(size = 0.64, colour =col) +
+    labs(title = "Labour Unit VS Total Production",  caption = "Cobb-Douglas Production Function") +
+    geom_point()+
+    scale_x_continuous(breaks = L) + xlab("Labour Unit")+ylab("Total Production")
+}
