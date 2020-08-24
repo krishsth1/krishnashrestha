7d6cf9b3200830ab266390391ecd876bca0383f2
Krishna Kumar Shrestha <45503377+krishsth1@users.noreply.github.com>
2020-08-24 22:36:09 +0545

    try3

--- Eco/R/create_table.R
+++ Eco/R/create_table.R
@@ -1,37 +1,34 @@
 #' it apply
 #' L vector of labour unit
-#' A 
+#' A
 #' K fixed capital
 #' @export
 
 
 
-create_table<-  function(L,A,K) 
+create_table<-  function(L,A,K)
   {
-  
-  
+
+
   library(dplyr)
-  
-  
+
+
   TP= A* sqrt(K) *sqrt(L)
-  
+
   data = data.frame(L,TP)
   data <- data %>% mutate(AP= TP/L)
   data<- data %>% mutate(MP= c(0,diff(TP)))
   data<- data %>% mutate(LM=c(0,(diff(L)/2)+L[-length(L)]))
-  
-  data <- data %>% mutate(DM=MP*4)
+
+  data <- data %>% mutate(TR=MP*4)
   data<- data %>% mutate(int=case_when(
-    DM > 56~"Profit",
-    round(DM,0) == 56 ~"Balance",
+    TM > 56~"Profit",
+    round(TM,0) == 56 ~"Balance",
     TRUE~"Loss"
   ))
-  table1<-data %>% select(L,TP,MP,AP,DM)
-  
-  table1<- table1 %>% rename("Labour Unit"=L,"Total Production"=TP,
-                             "Marginal Production"=MP,
-                             "Average Production"=AP,
-                             "Marginal production *4"=DM)
+  table1<-data %>% select(L,TP,MP,AP,TM)
+
+
   return(table1)
 }
 
